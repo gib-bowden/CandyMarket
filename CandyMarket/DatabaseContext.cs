d2f2bfaa6812499f80315cf79e76869ddeeebaa0
gib <gibbowden@gmail.com>
2018-02-03 13:51:27 -0600

    finished eat, clean, and give away

--- CandyMarket/DatabaseContext.cs
+++ CandyMarket/DatabaseContext.cs
@@ -5,54 +5,213 @@ using System.Linq;
 
 namespace CandyMarket
 {
-	internal class DatabaseContext
-	{
-		private int _countOfTaffy;
-		private int _countOfCandyCoated;
-		private int _countOfChocolateBar;
-		private int _countOfZagnut;
+    internal class DatabaseContext
+    {
+        private int _countOfTaffy;
+        private int _countOfCandyCoated;
+        private int _countOfChocolateBar;
+        private int _countOfZagnut;
 
-		/**
-		 * this is just an example.
-		 * feel free to modify the definition of this collection "BagOfCandy" if you choose to implement the more difficult data model.
-		 * Dictionary<CandyType, List<Candy>> BagOfCandy { get; set; }
-		 */
+        public Dictionary<string, int> CandyCounts = new Dictionary<string, int>();
 
-		public DatabaseContext(int tone) => Console.Beep(tone, 2500);
+        /**
+         * this is just an example.
+         * feel free to modify the definition of this collection "BagOfCandy" if you choose to implement the more difficult data model.
+         * Dictionary<CandyType, List<Candy>> BagOfCandy { get; set; }
+         */
 
-		internal List<string> GetCandyTypes()
-		{
-			return Enum
-				.GetNames(typeof(CandyType))
-				.Select(candyType =>
-					candyType.Humanize(LetterCasing.Title))
-				.ToList();
-		}
+        public DatabaseContext(int tone) => Console.Beep(tone, 2500);
 
-		internal void SaveNewCandy(char selectedCandyMenuOption)
-		{
-			var candyOption = int.Parse(selectedCandyMenuOption.ToString());
+        internal List<string> GetCandyTypes()
+        {
+            return Enum
+                .GetNames(typeof(CandyType))
+                .Select(candyType =>
+                    candyType.Humanize(LetterCasing.Title))
+                .ToList();
+        }
 
-			var maybeCandyMaybeNot = (CandyType)selectedCandyMenuOption;
-			var forRealTheCandyThisTime = (CandyType)candyOption;
+        internal void SaveNewCandy(char selectedCandyMenuOption)
+        {
+            var candyOption = int.Parse(selectedCandyMenuOption.ToString());
 
-			switch (forRealTheCandyThisTime)
-			{
-				case CandyType.TaffyNotLaffy:
-					++_countOfTaffy;
-					break;
-				case CandyType.CandyCoated:
-					++_countOfCandyCoated;
-					break;
-				case CandyType.CompressedSugar:
-					++_countOfChocolateBar;
-					break;
-				case CandyType.ZagnutStyle:
-					++_countOfZagnut;
-					break;
-				default:
-					break;
-			}
-		}
-	}
+            var maybeCandyMaybeNot = (CandyType)selectedCandyMenuOption;
+            var forRealTheCandyThisTime = (CandyType)candyOption;
+
+            switch (forRealTheCandyThisTime)
+            {
+                case CandyType.TaffyNotLaffy:
+                    ++_countOfTaffy;
+                    break;
+                case CandyType.CandyCoated:
+                    ++_countOfCandyCoated;
+                    break;
+                case CandyType.CompressedSugar:
+                    ++_countOfChocolateBar;
+                    break;
+                case CandyType.ZagnutStyle:
+                    ++_countOfZagnut;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        internal void EatCandy(char selectedCandyMenuOption)
+        {
+            var candyOption = int.Parse(selectedCandyMenuOption.ToString());
+
+            var eatenCandy = (CandyType)candyOption;
+
+            switch (eatenCandy)
+            {
+                case CandyType.TaffyNotLaffy:
+                    if (_countOfTaffy > 0)
+                    {
+                        --_countOfTaffy;
+                    }                    
+                    break;
+                case CandyType.CandyCoated:
+                    if (_countOfCandyCoated > 0)
+                    {
+                        --_countOfCandyCoated;
+                    }                    
+                    break;
+                case CandyType.CompressedSugar:
+                    if (_countOfChocolateBar > 0)
+                    {
+                        --_countOfChocolateBar;
+                    }                    
+                    break;
+                case CandyType.ZagnutStyle:
+                    if (_countOfZagnut > 0)
+                    {
+                        --_countOfZagnut;
+                    }                    
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        internal void TossCandy(char selectedCandyMenuOption)
+        {
+            var candyOption = int.Parse(selectedCandyMenuOption.ToString());
+
+            var eatenCandy = (CandyType)candyOption;
+
+            switch (eatenCandy)
+            {
+                case CandyType.TaffyNotLaffy:
+                    if (_countOfTaffy > 0)
+                    {
+                        _countOfTaffy = 0;
+                    }
+                    break;
+                case CandyType.CandyCoated:
+                    if (_countOfCandyCoated > 0)
+                    {
+                        _countOfCandyCoated = 0;
+                    }
+                    break;
+                case CandyType.CompressedSugar:
+                    if (_countOfChocolateBar > 0)
+                    {
+                        _countOfChocolateBar = 0;
+                    }
+                    break;
+                case CandyType.ZagnutStyle:
+                    if (_countOfZagnut > 0)
+                    {
+                        _countOfZagnut = 0;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        internal void GiveCandy(char selectedCandyMenuOption)
+        {
+            var candyOption = int.Parse(selectedCandyMenuOption.ToString());
+            var eatenCandy = (CandyType)candyOption;
+            int amount; 
+
+                switch (eatenCandy)
+                {
+                    case CandyType.TaffyNotLaffy:
+                    if (_countOfTaffy > 0)
+                    {
+                        amount = GiveAwayAmount(); 
+                        if (amount > 0)
+                        {
+                            var count = _countOfTaffy - amount;
+                            _countOfTaffy = (count < 0) ? 0 : count;
+                        }
+                    }
+                        break;
+                    case CandyType.CandyCoated:
+                    if (_countOfCandyCoated > 0)
+                    {
+                        amount = GiveAwayAmount();
+                        if (amount > 0)
+                        {
+                            var count = _countOfCandyCoated - amount;
+                            _countOfCandyCoated = (count < 0) ? 0 : count;
+                        }
+                    }
+                        break;
+                    case CandyType.CompressedSugar:
+                        if (_countOfChocolateBar > 0)
+                        {
+                        amount = GiveAwayAmount();
+                        if (amount > 0)
+                        {
+                            var count = _countOfChocolateBar - amount;
+                            _countOfChocolateBar = (count < 0) ? 0 : count;
+                        }
+
+                        }
+                        break;
+                    case CandyType.ZagnutStyle:
+                        if (_countOfZagnut > 0)
+                        {
+                        amount = GiveAwayAmount();
+                        if (amount > 0)
+                        {
+                            var count = _countOfZagnut - amount;
+                            _countOfZagnut = (count < 0) ? 0 : count;
+                        }
+
+                        }
+                        break;
+                    default:
+                        break;
+            }
+
+
+        }
+
+        internal Dictionary<string, int> GetCandyCounts()
+        {
+            CandyCounts.Clear();
+            CandyCounts.Add("TaffyNotLaffy", _countOfTaffy);
+            CandyCounts.Add("CandyCoated", _countOfCandyCoated);
+            CandyCounts.Add("CompressedSugar", _countOfChocolateBar);
+            CandyCounts.Add("ZagnutStyle", _countOfZagnut);
+
+            return CandyCounts;
+        }
+
+        internal int GiveAwayAmount()
+        {
+            Console.WriteLine();
+            Console.WriteLine("How much do you want to give away?");
+            var giveAway = Console.ReadLine();
+            var amount = int.Parse(giveAway.ToString());
+            return amount; 
+        }
+    }
 }
--- CandyMarket/Program.cs
+++ CandyMarket/Program.cs
@@ -4,47 +4,49 @@ using System.Linq;
 
 namespace CandyMarket
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			// wanna be a l33t h@x0r? skip all this console menu nonsense and go with straight command line arguments. something like `candy-market add taffy "blueberry cheesecake" yesterday`
-			var db = SetupNewApp();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // wanna be a l33t h@x0r? skip all this console menu nonsense and go with straight command line arguments. something like `candy-market add taffy "blueberry cheesecake" yesterday`
+            var db = SetupNewApp();
 
-			var run = true;
-			while (run)
-			{
-				ConsoleKeyInfo userInput = MainMenu();
+            var run = true;
+            while (run)
+            {
+                ConsoleKeyInfo userInput = MainMenu();
 
-				switch (userInput.KeyChar)
-				{
-					case '0':
-						run = false;
-						break;
-					case '1': // add candy to your bag
+                switch (userInput.KeyChar)
+                {
+                    case '0':
+                        run = false;
+                        break;
+                    case '1': // add candy to your bag
 
-						// select a candy type
-						var selectedCandyType = AddNewCandyType(db);
+                        // select a candy type
+                        var selectedCandyType = AddNewCandyType(db);
 
-						/** MORE DIFFICULT DATA MODEL
+                        /** MORE DIFFICULT DATA MODEL
 						 * show a new menu to enter candy details
 						 * it would be convenient to show the menu in stages e.g. press enter to go to next detail stage, but write the whole screen again with responses populated so far.
 						 */
 
-						// if(moreDifficultDataModel) bug - this is passing candy type right now (which just increments in our DatabaseContext), but should also be passing candy details
-						db.SaveNewCandy(selectedCandyType.KeyChar);
-						break;
-					case '2':
-						/** eat candy
+                        // if(moreDifficultDataModel) bug - this is passing candy type right now (which just increments in our DatabaseContext), but should also be passing candy details
+                        db.SaveNewCandy(selectedCandyType.KeyChar);
+                        break;
+                    case '2':
+                        /** eat candy
 						 * select a candy type
 						 * 
 						 * select specific candy details to eat from list filtered to selected candy type
 						 * 
 						 * enjoy candy
 						 */
-						break;
-					case '3':
-						/** throw away candy
+                        var candyToEat = EatSomeCandy(db);
+                        db.EatCandy(candyToEat.KeyChar);
+                        break;
+                    case '3':
+                        /** throw away candy
 						 * select a candy type
 						 * if(moreDifficultDataModel) enhancement - give user the option to throw away old candy in one action. this would require capturing the detail of when the candy was new.
 						 * 
@@ -52,9 +54,11 @@ namespace CandyMarket
 						 * 
 						 * cry for lost candy
 						 */
-						break;
-					case '4':
-						/** give candy
+                        var candyToToss = TossSomeCandy(db);
+                        db.TossCandy(candyToToss.KeyChar);
+                        break;
+                    case '4':
+                        /** give candy
 						 * feel free to hardcode your users. no need to create a whole UI to register users.
 						 * no one is impressed by user registration unless it's just amazingly fast & simple
 						 * 
@@ -63,56 +67,91 @@ namespace CandyMarket
 						 * you'll need a way to select what user you're giving candy to.
 						 * one design suggestion would be to put candy "on the table" and then "give the candy on the table" to another user once you've selected all the candy to give away
 						 */
-						break;
-					case '5':
-						/** trade candy
+                        var candyToGive = TossSomeCandy(db);
+                        db.GiveCandy(candyToGive.KeyChar);
+                        break;
+                    case '5':
+                        /** trade candy
 						 * this is the next logical step. who wants to just give away candy forever?
 						 */
-						break;
-					default: // what about requesting candy? like a wishlist. that would be cool.
-						break;
-				}
-			}
-		}
+                        break;
+                    default: // what about requesting candy? like a wishlist. that would be cool.
+                        break;
+                }
+            }
+        }
 
-		static DatabaseContext SetupNewApp()
-		{
-			Console.Title = "Cross Confectioneries Incorporated";
 
-			var cSharp = 554;
-			var db = new DatabaseContext(tone: cSharp);
+        static DatabaseContext SetupNewApp()
+        {
+            Console.Title = "Cross Confectioneries Incorporated";
 
-			Console.SetWindowSize(60, 30);
-			Console.SetBufferSize(60, 30);
-			Console.BackgroundColor = ConsoleColor.White;
-			Console.ForegroundColor = ConsoleColor.Black;
-			return db;
-		}
+            var cSharp = 554;
+            var db = new DatabaseContext(tone: cSharp);
 
-		static ConsoleKeyInfo MainMenu()
-		{
-			View mainMenu = new View()
-					.AddMenuOption("Did you just get some new candy? Add it here.")
-					.AddMenuOption("Do you want to eat some candy? Take it here.")
-					.AddMenuText("Press 0 to exit.");
+            Console.SetWindowSize(60, 30);
+            Console.SetBufferSize(60, 30);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            return db;
+        }
 
-			Console.Write(mainMenu.GetFullMenu());
-			ConsoleKeyInfo userOption = Console.ReadKey();
-			return userOption;
-		}
+        static ConsoleKeyInfo MainMenu()
+        {
+            View mainMenu = new View()
+                    .AddMenuOption("Did you just get some new candy? Add it here.")
+                    .AddMenuOption("Do you want to eat some candy? Take it here.")
+                    .AddMenuOption("Do you want to toss some candy? Do that shit here.")
+                    .AddMenuOption("Do you want to give some candy away? Do that shit here.")
+                    .AddMenuText("Press 0 to exit.");
 
-		static ConsoleKeyInfo AddNewCandyType(DatabaseContext db)
-		{
-			var candyTypes = db.GetCandyTypes();
+            Console.Write(mainMenu.GetFullMenu());
+            ConsoleKeyInfo userOption = Console.ReadKey();
+            return userOption;
+        }
 
-			var newCandyMenu = new View()
-					.AddMenuText("What type of candy did you get?")
-					.AddMenuOptions(candyTypes);
+        static ConsoleKeyInfo AddNewCandyType(DatabaseContext db)
+        {
+            var candyTypes = db.GetCandyTypes();
 
-			Console.Write(newCandyMenu.GetFullMenu());
+            var newCandyMenu = new View()
+                    .AddMenuText("What type of candy did you get?")
+                    .AddMenuOptions(candyTypes);
 
-			ConsoleKeyInfo selectedCandyType = Console.ReadKey();
-			return selectedCandyType;
-		}
-	}
+            Console.Write(newCandyMenu.GetFullMenu());
+
+            ConsoleKeyInfo selectedCandyType = Console.ReadKey();
+            return selectedCandyType;
+        }
+
+        static ConsoleKeyInfo EatSomeCandy(DatabaseContext db)
+        {
+            var candyTypes = db.GetCandyTypes();
+            var candyCount = db.GetCandyCounts();
+
+            var newCandyMenu = new View()
+                    .AddMenuText("What type of candy do you want to eat?")
+                    .RemoveMenuOptions(candyCount);
+
+            Console.Write(newCandyMenu.GetFullMenu());
+
+            ConsoleKeyInfo selectedCandyType = Console.ReadKey();
+            return selectedCandyType;
+        }
+
+        static ConsoleKeyInfo TossSomeCandy(DatabaseContext db)
+        {
+            var candyTypes = db.GetCandyTypes();
+            var candyCount = db.GetCandyCounts();
+
+            var newCandyMenu = new View()
+                .AddMenuText("What type of candy do you want to toss?")
+                .RemoveMenuOptions(candyCount);
+
+            Console.Write(newCandyMenu.GetFullMenu());
+
+            ConsoleKeyInfo selectedCandyType = Console.ReadKey();
+            return selectedCandyType;
+        }
+    }
 }
